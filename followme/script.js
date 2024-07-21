b599f2510f4ab343d58f837084e9dde9d8481ea0
Lemoxyy <fclan3809@gmail.com>
2024-07-21 23:20:16 +0545

    12

--- followme/script.js
+++ followme/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('click', () => {
     document.getElementById("song").play();
+    // document.getElementById("song").pause();
     document.getElementById("song").volume = -200;
 })
