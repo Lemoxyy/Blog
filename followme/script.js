2c4fef8029949253827ebdee0b1cfab1fc08def9
Lemoxyy <fclan3809@gmail.com>
2024-07-22 01:29:58 +0545

    time

--- followme/script.js
+++ followme/script.js
@@ -18,3 +18,23 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime();  // initial call to set the time immediately
+
+
+// script.js
+
+document.getElementById('fb-btn').addEventListener('click', function() {
+    window.open('https://www.facebook.com/lemoxyy', '_blank');
+});
+
+document.getElementById('x-btn').addEventListener('click', function() {
+    window.open('https://x.com/Lemoxyy_', '_blank');
+});
+document.getElementById('thread-btn').addEventListener('click', function() {
+    window.open('https://www.threads.net/@lemoxyy__', '_blank');
+});
+document.getElementById('insta-btn').addEventListener('click', function() {
+    window.open('https://www.instagram.com/lemoxyy__', '_blank');
+});
+document.getElementById('discord-btn').addEventListener('click', function() {
+    window.open('https://discordapp.com/users/669179511186915344', '_blank');
+});
